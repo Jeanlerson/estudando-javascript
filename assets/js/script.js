a278067bc618bc674438b2a94bc1db6affe6d11b
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2023-06-06 13:18:48 -0300

    estudando-JavaScript-funções

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,46 @@ let y = 10;
 let pct = calcPct(y, x);
 
 console.log(`${pct}% de ${x} é ${y}`)
+
+function calcularImovel(metragem, quartos) {
+    let m2 = 3000;
+    let preco = 0;
+
+    switch (quartos) {
+        case 1:
+        default:
+            preco = metragem * m2;
+            break;
+        case 2:
+            preco = metragem * (m2 * 1.2);
+            break;
+        case 3:
+            preco = metragem * (m2 * 1.5);
+            break;
+    }
+
+    return preco;
+}
+
+let metragem = 123;
+let quartos = 3;
+let preco = calcularImovel(metragem, quartos);
+console.log(`A casa custa R$ ${preco}`)
+
+
+function validar(usuario, senha) {
+    if (usuario = "pedro" && senha === "123") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let usuario = 'bonieky';
+let senha = '1234';
+let validacao = validar(usuario, senha);
+if (validacao) {
+    console.log('Acesso concedido.');
+} else {
+    console.log('Acesso NEGADO!');
+}
