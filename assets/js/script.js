5aa0bdc94f6e4fa4fed091de6c63b6b5baad1a0c
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2023-06-14 14:45:48 -0300

    Eventos-de-Clique

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,8 @@
-/*let cars = [
-    { brand: 'Fiat', year: 2001},
-    { brand: 'Fiat', year: 2010},
-    { brand: 'Fiat', year: 2011},
-    { brand: 'Fiat', year: 2023}
-];
-
-cars.sort((a, b) => {
-    if (a.year > b.year) {
-        return 1;
-    } else if (a.year < b.year) {
-        return -1;
-    } else {
-        return 0
-    }
-});
-
-console.log(cars);*/
-
-let fruits = ['Banana', 'Laranja', 'Maça', 'Uva', 'Acerola'];
-
-let bigFruits = fruits.some((value) => value.length > 4);
-
-fruits.pop()
-
-if (fruits.includes('Acerola')) {
-    console.log('Tem Acerola')
-} else {
-    console.log('Não tem Acerola')
+function clicou() {
+    console.log("Clicou no botão!")
 }
+
+let botao = document.querySelector(".botao");
+botao.addEventListener("click", function() {
+    clicou();
+})
