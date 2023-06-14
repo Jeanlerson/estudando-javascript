b4ca2aa6f4ecc6403831cf1cc51ac7a1fbb88c4a
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2023-06-14 17:02:43 -0300

    ManipulandoElementos

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,13 @@ function clicou(){
     const teste = document.querySelector("#teste1");
     const ul = teste.querySelector("ul");
 
-    let newli = document.createElement("li");
-    newli.innerText = "item novo";
 
-    ul.appendChild(newli)
+    let newUl = document.createElement("ul")
+    for(let x = 1; x < 6; x++){
+        let newLi = document.createElement("li")
+        newLi.innerHTML = "Item add " + x;
+        newUl.append(newLi);
+    }
+
+    ul.after(newUl);
 }
