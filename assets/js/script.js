a4e94e56689d7e3960d4616a56a6a377136eaebe
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2023-06-15 14:14:37 -0300

    Manipulando-CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,7 @@
 function clicou(){
-    const input_text = document.querySelector(".input_text");
-    const botao = document.querySelector(".botao")
+    const teste = document.querySelector('#teste');
 
-    if(input_text.getAttribute('type') === 'text') {
-        input_text.setAttribute('type', 'password')
-        botao.innerHTML = "Mostrar Senha"
-    }
-    else {
-        input_text.setAttribute('type', 'text')
-        botao.innerHTML = "Ocultar Senha"
-    }
+    teste.style.backgroundColor = "#000"
+    teste.style.color = "#fff"
+    teste.style.fontSize = "20px"
 }
