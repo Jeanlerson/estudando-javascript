874835fcae04f2eaf5b8ecd42d83998f15c55f35
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2023-06-16 13:36:15 -0300

    Eventos-de-Teclado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-function clicou(){
-    const teste = document.querySelector('#teste');
-
-    teste.style.backgroundColor = "#000"
-    teste.style.color = "#fff"
-    teste.style.fontSize = "20px"
+function soltou(e) {
+    console.log('TECLA APERTADA ' + e.code );
+    console.log('SHIFT? ' + e.shiftKey);
+    console.log('CRTL? ' + e.ctrlKey)
+    console.log('ATL? ' + e.altKey)
+    console.log('----')
 }
+
+const input = document.querySelector('input');
+input.addEventListener('keyup', soltou);
